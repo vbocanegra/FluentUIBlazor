d7b15725ec3294f7e28014148ede700140153d97
cesquen <cesquen@barredamoller.com>
2023-12-12 09:39:57 -0500

    cambio 12

---
+++ BlazorApp/Dtos/PersonDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Dtos
+{
+    public class PersonDto
+    {
+        public bool Selected { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; } = "n/a";
+        public string Country { get; set; } = "n/a";
+        public int Age { get; set; }
+    }
+}
---
+++ BlazorApp/Services/CustomerServices.cs
@@ -0,0 +1,32 @@
+﻿using BlazorApp.Dtos;
+
+namespace BlazorApp.Services
+{
+    public class CustomerServices
+    {
+
+        public static List<PersonDto> GetCustomers() => new()
+        {
+            new() { Selected=false, Id = 1, Name = "Alice", Country = "USA", Age = 25 },
+            new() {Selected=false, Id = 2, Name = "Bob", Country = "Canada", Age = 32 },
+            new() {Selected=false, Id = 3, Name = "Charlie", Country = "Mexico", Age = 45 },
+            new() {Selected=false, Id = 4, Name = "David", Country = "Germany", Age = 22 },
+            new() {Selected=false, Id = 5, Name = "Emma", Country = "France", Age = 39 },
+            new() {Selected=false, Id = 6, Name = "Frank", Country = "UK", Age = 28 },
+            new() {Selected=false, Id = 7, Name = "Grace", Country = "Australia", Age = 41 },
+            new() {Selected=false, Id = 8, Name = "Henry", Country = "Russia", Age = 50 },
+            new() {Selected=false, Id = 9, Name = "Isabella", Country = "Brazil", Age = 29 },
+            new() {Selected=false, Id = 10, Name = "Jack", Country = "China", Age = 19 },
+            new() {Selected=false, Id = 11, Name = "Katherine", Country = "India", Age = 36 },
+            new() {Selected=false, Id = 12, Name = "Lucas", Country = "Spain", Age = 27 },
+            new() {Selected=false, Id = 13, Name = "Mia", Country = "Argentina", Age = 31 },
+            new() {Selected=false, Id = 14, Name = "Noah", Country = "Japan", Age = 24 },
+            new() {Selected=false, Id = 15, Name = "Olivia", Country = "Italy", Age = 42 },
+            new() {Selected=false, Id = 16, Name = "Patrick", Country = "South Korea", Age = 38 },
+            new() {Selected=false, Id = 17, Name = "Quinn", Country = "South Africa", Age = 47 },
+            new() {Selected=false, Id = 18, Name = "Ryan", Country = "Nigeria", Age = 26 },
+            new() {Selected=false, Id = 19, Name = "Sophia", Country = "Egypt", Age = 33 },
+            new() {Selected=false, Id = 20, Name = "Thomas", Country = "Kenya", Age = 30 }
+        };
+    }
+}
